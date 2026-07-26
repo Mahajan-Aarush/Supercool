eb4d37af93fc67ecf52913d8ade4b05ed8365f12
Mahajan-Aarush <eagluuuu@gmail.com>
2026-07-26 22:22:33 +0530

    ..

---
+++ Assets/Scripts/Player/player.cs
@@ -0,0 +1,26 @@
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+
+
+    void Start()
+    {
+        
+    }
+
+
+
+
+
+    
+
+
+
+
+
+}
